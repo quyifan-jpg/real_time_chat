41ae1274b5f3f702560c02bb87edb2a173823c05
unknown <quyifan@gmail.com>
2022-05-03 23:36:06 -0400

    test

--- Messanger/App.tsx
+++ Messanger/App.tsx
@@ -18,6 +18,9 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
+
+
+        
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
       </SafeAreaProvider>
