4b24a9604f9568589a57c52e78e69029c88cf3e1
unknown <quyifan@gmail.com>
2022-06-18 14:51:01 -0400

    initial

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+import Navigation from './components/Navigation';
+
+function App() {
+  return (
+    <>
+    <Navigation></Navigation>
+    
+    </>
+  );
+}
+
+export default App;
